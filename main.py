ef8708567917a929b452dc66af27b8fe60ca8774
OMAN <84547139+OmanDominik@users.noreply.github.com>
2021-05-28 17:19:41 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from GUI import *
-
-if __name__ == '__main__':
-    "Uruchamianie aplikacji"
-
-    root = tk.Tk()
-    root.resizable(False, False)
-    exchange_app = Application(master=root)
-    exchange_app.mainloop()
