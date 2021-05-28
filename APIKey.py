7eea61208bbd29e91f61504d375c23e71d10160e
OMAN <84547139+OmanDominik@users.noreply.github.com>
2021-05-28 17:24:39 +0200

    Update APIKey.py

--- APIKey.py
+++ APIKey.py
@@ -2,5 +2,5 @@
 Plik zawierający klucz do API oraz adres hosta API.
 """
 
-API_KEY = "265d561b68msh492e979bf87fd4dp1d17f7jsn9ffac106720d"
+API_KEY = "ukryty"
 API_HOST = "apidojo-yahoo-finance-v1.p.rapidapi.com"
